93d7ac38c8ad91f6771fb8fae023682e75751571
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-10-07 04:27:42 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 supports "ubuntu", ">= 12.04"
-supports "amazon", ">= 2014.03"
 
 depends "awscli", ">= 0.4.0"
 

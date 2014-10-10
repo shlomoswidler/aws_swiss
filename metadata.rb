9bd97ba18598742a9a6664978dc021d6d9ad80f7
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-10-10 12:53:42 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'rds_swiss'
+name             'aws_swiss'
 maintainer       'Shlomo Swidler'
 maintainer_email 'shlomo.swidler@orchestratus.com'
 license          'Apache 2.0'
-description      "Dynamically poke holes in EC2-Classic RDS instances' security groups"
+description      "Dynamically poke holes in EC2 and RDS security groups"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports "ubuntu", ">= 12.04"
 

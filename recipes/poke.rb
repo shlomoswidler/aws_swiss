16328508b85088e32094425e47705f5dfeb0420c
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-10-10 14:28:29 +0300

    Update poke.rb

--- recipes/poke.rb
+++ recipes/poke.rb
@@ -1,7 +1,7 @@
 aws_swiss node[:aws_swiss][:security_group] do
   aws_access_key_id     node[:aws_swiss][:aws_access_key_id]
   aws_secret_access_key node[:aws_swiss][:aws_secret_access_key]
-  rds_name              node[:aws_swiss][:rds_name] # May be nil - but at least one of 'ports' or 'rds_name' must be given
-  ports                 node[:aws_swiss][:ports]    # May be nil - but at least one of 'ports' or 'rds_name' must be given
+  rds_name              node[:aws_swiss][:rds_name] # May be nil - but at least one of 'port' or 'rds_name' must be given
+  port                  node[:aws_swiss][:port]     # May be nil - but at least one of 'port' or 'rds_name' must be given
   enable                true
 end

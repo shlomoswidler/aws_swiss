66ea2c9e59201e4442ad8441a1567db8120a9a01
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-11-26 16:40:13 +0200

    refactor

--- libraries/security_group_hole_controller.rb
+++ libraries/security_group_hole_controller.rb
@@ -1,7 +1,17 @@
 module SecurityGroupHoleController
   
   # private
-  def self.detect_rds_hole(command_base, security_group, cidr)
+  def self.awscli_command_stem(region, aws_access_key_id, aws_secret_access_key)
+    command_base = ""
+    if !aws_access_key_id.nil? && !aws_secret_access_key.nil?
+      command_base = "AWS_ACCESS_KEY_ID=#{aws_access_key_id} AWS_SECRET_ACCESS_KEY=#{aws_secret_access_key} "
+    end
+    command_base << "/usr/local/bin/aws --region #{region} "
+    command_base
+  end
+  
+  # private
+  def self.detect_rds_hole(security_group, cidr, region, aws_access_key_id, aws_secret_access_key)
     str=%x^#{command_base} rds describe-db-security-groups --db-security-group-name #{security_group}^
     json=JSON.parse(str)
     # the return value - array of [ hole_exists, num_holes ]
@@ -11,14 +21,10 @@ module SecurityGroupHoleController
   end
   
   def self.open_rds_hole_if_necessary(security_group, cidr, region, aws_access_key_id, aws_secret_access_key)
-    command_base = ""
-    if !aws_access_key_id.nil? && !aws_secret_access_key.nil?
-      command_base = "AWS_ACCESS_KEY_ID=#{aws_access_key_id} AWS_SECRET_ACCESS_KEY=#{aws_secret_access_key} "
-    end
-    command_base << "/usr/local/bin/aws --region #{region} "
     
-    hole_exists, num_holes = PRIVATE_detect_rds_hole(command_base, security_group, cidr)
+    hole_exists, num_holes = detect_rds_hole(security_group, cidr, region, aws_access_key_id, aws_secret_access_key)
     if !hole_exists
+      command_base = awscli_command_stem(region, aws_access_key_id, aws_secret_access_key)
       shell = Mixlib::ShellOut.new(command_base + "rds authorize-db-security-group-ingress --db-security-group-name #{security_group} --cidrip #{cidr}")
       shell.run_command
       if shell.exitstatus != 0

6b912b6868456dc6311b66b7eaaf85d14598b493
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-11-26 13:50:14 +0200

    remove rds_name param

--- recipes/poke.rb
+++ recipes/poke.rb
@@ -1,8 +1,7 @@
 aws_swiss node[:aws_swiss][:security_group] do
   aws_access_key_id     node[:aws_swiss][:aws_access_key_id]
   aws_secret_access_key node[:aws_swiss][:aws_secret_access_key]
-  rds_name              node[:aws_swiss][:rds_name] # May be nil - but at least one of 'port' or 'rds_name' must be given
-  port                  node[:aws_swiss][:port]     # May be nil - but at least one of 'port' or 'rds_name' must be given
+  port                  node[:aws_swiss][:port]     # May be nil
   enable                true
   fallback_group        node[:aws_swiss][:fallback_group] # May be nil
 end

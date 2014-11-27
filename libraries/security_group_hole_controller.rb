e7e2684d794ce04059b19d3c14951abe6ca62f33
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-11-27 22:50:47 +0200

    refactor

--- libraries/security_group_hole_controller.rb
+++ libraries/security_group_hole_controller.rb
@@ -13,15 +13,22 @@ module SecurityGroupHoleController
     security_group[/sg-([0-9a-f]{8})/, 1].nil? ? "group-name" : "group-id"
   end
   
-  # private
-  def self.detect_rds_hole(security_group, cidr, region, aws_access_key_id, aws_secret_access_key)
+  def self.get_rds_cidr_holes(security_group, region, aws_access_key_id, aws_secret_access_key)
     command_base = awscli_command_stem(region, aws_access_key_id, aws_secret_access_key)
     str=%x^#{command_base} rds describe-db-security-groups --db-security-group-name #{security_group}^
     json=JSON.parse(str)
+    json['DBSecurityGroups'].first['IPRanges'].reduce([]) { |result, cidrHash| 
+      if ["authorized", "authorizing"].include?(cidrHash['Status'])
+        result << cidrHash['CIDRIP']
+      end
+      result
+    }
+  end
+  
+  def self.detect_rds_hole(security_group, cidr, region, aws_access_key_id, aws_secret_access_key)
+    cidr_holes = get_rds_cidr_holes(security_group, region, aws_access_key_id, aws_secret_access_key)
     # the return value - array of [ hole_exists, num_holes ]
-    [ json['DBSecurityGroups'].first['IPRanges'].any? { | cidrHash | 
-      cidrHash['CIDRIP'] == cidr && ["authorized", "authorizing"].include?(cidrHash['Status']) },
-      json['DBSecurityGroups'].first['IPRanges'].size ]
+    [ cidr_holes.include?(cidr), cidr_holes.size ]
   end
   
   def self.open_rds_hole_if_necessary(security_group, cidr, region, aws_access_key_id, aws_secret_access_key)
@@ -75,4 +82,5 @@ module SecurityGroupHoleController
       true
     end
   end
+  
 end

e544f7bff00ddb689e053e57f5b4f89140c241f2
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-11-28 12:23:29 +0200

    Create enforce.rb

---
+++ recipes/enforce.rb
@@ -0,0 +1,17 @@
+unless node[:aws_swiss][:port].nil?
+
+  this_layers_cidrs = node[:opsworks][:layers][ node[:opsworks][:instance][:layers].first ][:instances].reduce([]) { |result, instance|
+    result << (instance.values.first[:ip] + "/32")
+    result
+  }
+  
+  Chef::Log.info("cidrs in this layer: #{this_layers_cidrs.join(',')}")
+
+  swiss_rds_enforcer do
+    aws_access_key_id     node[:aws_swiss][:aws_access_key_id]
+    aws_secret_access_key node[:aws_swiss][:aws_secret_access_key]
+    region                node[:opsworks][:instance][:region]
+    cidr_list             this_layers_cidrs
+    security_group_list   [ node[:aws_swiss][:security_group], node[:aws_swiss][:fallback_group] ]
+  end
+end

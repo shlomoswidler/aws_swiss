c21ed3b1b9fba04bfdfe996ffa9b243df21195af
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-11-29 22:12:55 +0200

    Actually plug and poke

--- definitions/swiss_rds_enforcer.rb
+++ definitions/swiss_rds_enforcer.rb
@@ -3,12 +3,16 @@ define :swiss_rds_enforcer, \
   :aws_secret_access_key => nil, \
   :region => nil, \
   :cidr_list => nil, \
-  :security_group_list => nil \
+  :security_group => nil, \
+  :fallback_group => nil \
 do
-
-  security_group_list = params[:security_group_list]
-  if security_group_list.is_a? String
-    security_group_list = security_group_list.split(',')
+  if params[:security_group].nil?
+    raise "swiss_rds_enforcer definition: security_group must be specified"
+  end
+  
+  security_group_list = [ params[:security_group] ]
+  if params[:fallback_group]
+    security_group_list << params[:fallback_group]
   end
   cidr_list = params[:cidr_list]
   if cidr_list.is_a? String
@@ -27,12 +31,15 @@ do
       confirmed_holes, extra_holes = holes.partition {|cidr| cidr_list.include?(cidr) }
       more_confirmed_holes, missing_holes = cidr_list.partition {|cidr| holes.include?(cidr) }
       
-      # TODO: plug extra_holes (may require moving functionality into the library module)
-      Chef::Log.info("#{extra_holes.size} extra holes: #{extra_holes.sort.join(',')}")
+      Chef::Log.info("Plugging #{extra_holes.size} extra holes: #{extra_holes.sort.join(',')}")
+      extra_holes.each { |cidr|
+        SecurityGroupHoleController.close_rds_hole_with_fallback(params[:security_group], params[:fallback_group], cidr, params[:region], params[:aws_access_key_id], params[:aws_secret_access_key])
+      }
       
-      # TODO: poke missing_holes (likewise)
-      Chef::Log.info("#{missing_holes.size} missing holes: #{missing_holes.sort.join(',')}")
-      
+      Chef::Log.info("Poking #{missing_holes.size} missing holes: #{missing_holes.sort.join(',')}")
+      missing_holes.each { |cidr|
+        SecurityGroupHoleController.open_rds_hole_with_fallback(params[:security_group], params[:fallback_group], cidr, params[:region], params[:aws_access_key_id], params[:aws_secret_access_key])
+      }
     end
   end
   
